7f613148b3124504b35857915f2cf2226281960a
om32-web <oussama.mahmoud19@hotmail.com>
2025-01-31 15:31:19 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,10 @@
+function togglePopup() { /* a popup */
+    var popup = document.getElementById("popup");
+    popup.style.display = (popup.style.display === "block") ? "none" : "block";
+}
+
+function navigate() {
+    window.location.href = "another-page.html"; // Change this to the path of the page you want to navigate to.
+}
+
+
